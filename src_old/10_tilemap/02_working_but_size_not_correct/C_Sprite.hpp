e0584bba26ed53af1ac5bdf47e5698b1f80166a5
Kamalyesh Kannadkar <kkamalyesh@gmail.com>
2021-05-16 22:04:22 +0530

    tilemap not drawn issue solved
    tilemap not drawn issue solved

---
+++ src_old/10_tilemap/02_working_but_size_not_correct/C_Sprite.hpp
@@ -0,0 +1,32 @@
+#ifndef ComponentSprite_hpp
+#define ComponentSprite_hpp
+
+#include "Component.hpp"
+#include "C_Transform.hpp"
+#include "Object.hpp"
+#include "ResourceAllocator.hpp"
+
+class ComponentSprite : public Component
+{
+public:
+    ComponentSprite(Object* owner);
+    
+    void SetTextureAllocator(ResourceAllocator<sf::Texture>* allocator);
+    
+    void Load(int id);
+    void Load(const std::string& filePath);
+    
+    void LateUpdate(float deltaTime) override;
+    void Draw(Window& window) override;
+    
+    void SetTextureRect(int x, int y, int width, int height);
+    void SetTextureRect(const sf::IntRect& rect);
+    void SetScale(float scaleX, float scaleY);
+    
+private:
+    ResourceAllocator<sf::Texture>* allocator;
+    sf::Sprite sprite;
+    int currentTextureID;
+};
+
+#endif /* ComponentSprite_hpp */
---
+++ src_old/10_tilemap/02_working_but_size_not_correct/GameLoopMain.hpp
@@ -0,0 +1,12 @@
+#ifndef GameLoopMain_hpp
+#define GameLoopMain_hpp
+
+#include "Game.hpp"
+
+class GameLoopMain
+{
+public:
+    int main();
+};
+
+#endif /* GameLoopMain_hpp */
